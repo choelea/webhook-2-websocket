5b277171144433c3544cfa8678e54e3f46f8a624
Joe Li <joe.lea@foxmail.com>
2020-06-19 22:13:39 +0800

    refine deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,16 +23,16 @@ module.exports = {
       user : 'root',
       host : '121.42.12.209',
       ref  : 'origin/master',
-      repo : 'git@github.com:choelea/markdown-cms.git',
-      path : '/root/apps/markdown-cms',
+      repo : 'git@github.com:choelea/webhook-2-websocket.git',
+      path : '/root/apps/webhook-debuger',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     prd : { // quick deploy with out npm install
       user : 'root',
       host : '121.42.12.209',
       ref  : 'origin/master',
-      repo : 'git@github.com:choelea/markdown-cms.git',
-      path : '/root/apps/markdown-cms',
+      repo : 'git@github.com:choelea/webhook-2-websocket.git',
+      path : '/root/apps/webhook-debuger',
       'post-deploy' : 'pm2 reload ecosystem.config.js --env production'
     }
   }
